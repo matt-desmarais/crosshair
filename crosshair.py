b3194e210932a19e496c277311b057272ed2fc56
Matt <matt.desmarais78@gmail.com>
2016-05-10 11:10:00 -0400

    Update crosshair.py

--- crosshair.py
+++ crosshair.py
@@ -7,7 +7,6 @@ if cmd_folder not in sys.path:
 cmd_subfolder = os.path.realpath(os.path.abspath(os.path.join(os.path.split(inspect.getfile( inspect.currentframe() ))[0],"include")))
 if cmd_subfolder not in sys.path:
     sys.path.insert(0, cmd_subfolder)
-sys.path.append('/usr/local/lib/python2.7/site-packages')
 
 import time
 import picamera
