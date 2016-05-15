3a1b8b3a51bb202e8113258cbf889d868c7bdd4a
Matt <matt.desmarais78@gmail.com>
2016-05-15 11:33:14 -0400

    Update crosshair.py

--- crosshair.py
+++ crosshair.py
@@ -257,33 +257,43 @@ def patternswitch(target,guitoggle):
     if curpat == 1:
         patterns.pattern5(target, width, height, xcenter, ycenter-10, radius, col)
         gunRange = 25
+        creategui(gui)
     if curpat == 2:
         patterns.pattern5(target, width, height, xcenter, ycenter-20, radius, col)
         gunRange = 20
+        creategui(gui)
     if curpat == 3:
         patterns.pattern5(target, width, height, xcenter, ycenter-30, radius, col)
         gunRange = 15
+        creategui(gui)
     if curpat == 4:
         patterns.pattern5(target, width, height, xcenter, ycenter-40, radius, col)
         gunRange = 10
+        creategui(gui)
     if curpat == 5:
         patterns.pattern5(target, width, height, xcenter, ycenter, radius, col)
         gunRange = 30
+        creategui(gui)
     if curpat == 6:
         patterns.pattern5(target, width, height, xcenter, ycenter+10, radius, col)
         gunRange = 40
+        creategui(gui)
     if curpat == 7:
         patterns.pattern5(target, width, height, xcenter, ycenter+20, radius, col)
         gunRange = 50
+        creategui(gui)
     if curpat == 8:
         patterns.pattern5(target, width, height, xcenter, ycenter+30, radius, col)
         gunRange = 60
+        creategui(gui)
     if curpat == 9:
         patterns.pattern5(target, width, height, xcenter, ycenter+40, radius, col)
         gunRange = 70
+        creategui(gui)
     if curpat == 10:
         patterns.pattern5(target, width, height, xcenter, ycenter, radius, col)
         gunRange = 30
+        creategui(gui)
     
     # Add the overlay directly into layer 3 with transparency;
     # we can omit the size parameter of add_overlay as the
